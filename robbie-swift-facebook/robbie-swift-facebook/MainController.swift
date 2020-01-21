ce839375024a0095f65ddfce27bd3ab8628ed88d
Robbie Gay <robbiecgay@gmail.com>
2020-01-21 11:21:08 -0500

    Finished Ep 3 Stories Header

--- robbie-swift-facebook/robbie-swift-facebook/MainController.swift
+++ robbie-swift-facebook/robbie-swift-facebook/MainController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+//import SwiftUI
 import LBTATools
 
 class PostCell: LBTAListCell<String> {
@@ -27,10 +28,96 @@ class PostCell: LBTAListCell<String> {
               postTextLabel,
               photosGridController.view,
               spacing: 12)
+        
     }
 }
 
-class MainController: LBTAListController<PostCell, String>, UICollectionViewDelegateFlowLayout {
+
+class StoryHeader: UICollectionReusableView {
+    
+    let storiesController = StoriesController(scrollDirection: .horizontal)
+    
+    override init(frame: CGRect) {
+        super .init(frame: frame)
+        backgroundColor = .yellow
+        
+        stack(storiesController.view)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+}
+
+class StoryPhotoCell: LBTAListCell<String> {
+    
+    override var item: String! {
+        didSet {
+            imageView.image = UIImage(named: item)
+        }
+    }
+    
+    let imageView = UIImageView(image: nil, contentMode: .scaleAspectFill)
+    
+    let nameLabel = UILabel(text: "Robbie Gay", font: .boldSystemFont(ofSize: 12), textColor: .white)
+    
+    override func setupViews() {
+//        backgroundColor = .red
+        
+        imageView.layer.cornerRadius = 10
+        
+        stack(imageView)
+        
+        setupGradientLayer()
+        
+        stack(UIView(), nameLabel).withMargins(.allSides(8))
+    }
+    
+    let gradientLayer = CAGradientLayer()
+    
+    fileprivate func setupGradientLayer() {
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0.5, 1]
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        
+        layer.addSublayer(gradientLayer)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        gradientLayer.frame = bounds
+    }
+    
+}
+
+class StoriesController: LBTAListController<StoryPhotoCell, String>, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: 100, height: view.frame.height - 24)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 0, left: 12, bottom: 0, right: 12)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.items = ["headshot-short-hair", "Bootcamp Headshot Robbie", "Robbie Rushmore Parking Garage 2", "headshot-short-hair", "Bootcamp Headshot Robbie", "Robbie Rushmore Parking Garage 2"]
+    }
+}
+
+class MainController: LBTAListHeaderController<PostCell, String, StoryHeader>, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .init(width: 0, height: 200)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 12, left: 0, bottom: 0, right: 0)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
